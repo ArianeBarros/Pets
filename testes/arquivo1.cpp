46c91487a8a58dae3a21e09b3bca85d8fd85a201
Leonardo Hana Bersello <leonardomx111@gmail.com>
2021-11-04 12:20:18 -0300

    Modified main file pet.cpp and its exe file. And added a new folder called testes with some test files. Modications in pet.cpp file:  - Changed all strings to char arrays.  - Added some comments for future updates.  - Added comments to create and read file, but it is far from complete.
    Addition of folder testes:
     - Added a new file called arquivo1.cpp that is Nakamiti's lesson.

---
+++ testes/arquivo1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+  int num;
+  char linha[20];
+  FILE *arq;
+
+  if ((arq = fopen("Arquivo.txt", "r+")) == NULL){
+    printf("Arquivo não existente. Criacao do arquivo\n");
+    arq= fopen ("Arquivo.txt", "w+");
+
+    printf("Digite um numero para inserir no arquivo: ");
+    scanf("%d",&num);
+    fprintf(arq, "%d", num);
+    fprintf(arq,"%c", ' ');
+    printf("Digite uma palavra para inserir no arquivo: ");
+    scanf("%s",linha);
+    fprintf(arq, "%s", linha);
+
+  }
+    else{
+      printf("O arquivo ja existe\n");
+      fscanf(arq,"%d", &num);
+      printf("Numero lido do arquivo: %d\n", num);
+      fscanf(arq,"%s", linha);
+      printf("Palavra lida do arquivo: %s\n", linha);
+
+    }
+
+
+  fclose(arq);
+}
